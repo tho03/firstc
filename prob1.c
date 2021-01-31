5441a3ce3b5c4a91a749010d92a12f9d13f6d6e2
Thuan Ho <tho03@g.uafs.edu>
2021-01-31 01:37:48 +0000

    Added prob1

---
+++ prob1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*variables*/
+	char ch,h = '-';
+	/*read*/
+	ch = getchar();
+	/*print*/
+	putchar (ch);
+	putchar	(h);
+	putchar (ch);
+
+	return 0;
+
+}
