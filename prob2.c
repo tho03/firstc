cfef3e3af3b31e987d63ec10aa23ad8bc634d148
Thuan Ho <tho03@g.uafs.edu>
2021-01-31 02:32:55 +0000

    Added prob2

---
+++ prob2.c
@@ -0,0 +1,52 @@
+#include <stdio.h> 
+
+int main (void){
+
+	char ch,h = ' ';
+
+	ch = getchar();
+	
+	putchar('\n');
+	putchar(ch);
+	putchar(h);
+	putchar(ch);
+	putchar(h);
+	putchar(ch);
+	putchar(h);
+	putchar(ch);
+	putchar(h);
+
+        putchar('\n');
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+	
+	putchar('\n');
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+
+	putchar('\n');
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+        putchar(ch);
+        putchar(h);
+		
+	return 0;
+
+
+}
